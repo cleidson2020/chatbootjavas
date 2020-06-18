fd9d9adf62d786d274f3f158ffe5bf619f7eafe0
Cleidson Silva <contatosilva062001@gmail.com>
2020-06-17 22:11:26 -0300

    Primeiro Commit

---
+++ meuScrip.js
@@ -0,0 +1,29 @@
+alert("Bem vindo ao CineTorrent");
+
+
+let planos = 100;
+
+let nome = window.prompt("Digite seu Nome ");
+let email = window.prompt("Digite seu E-mail");
+let criarSenha = window.prompt("Criar uma Senha");
+
+
+alert("E-mail e Senha Cadastrados Com Sucesso");
+alert("Plano Anual R$100");
+
+let planoAnual = window.prompt("Digite Seu Plano");
+
+if (planoAnual == planos ) {
+
+    planoAnual == planos;
+    
+  window.prompt("Digire A senha do Cartão");
+  window.prompt("Data de Validade");
+  window.prompt("CVV");
+
+}
+else{
+
+    alert("Plano Não Existe");
+
+}
